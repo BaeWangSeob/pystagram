0031f3beb02779ff8eb9986b4718d268fdd4dd3d
WangSeob <bys326@naver.com>
2016-07-13 14:12:53 +0900

    4챕터까지 진행

--- photo/models.py
+++ photo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Photo (models.Model):
-	image_file = models.ImageField()
-	filtered_image_file = models.ImageField()
-	description = models.TextField(max_length=500)
+	image_file = models.ImageField(upload_to='static_files/uploaded/%Y/%m/%d')
+	filtered_image_file = models.ImageField(upload_to='static_files/uploaded/filtered/%Y/%m/%d')
+	description = models.TextField(max_length=500, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 
-	
+
